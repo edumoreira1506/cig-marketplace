69460dda9612935a87739ead7b43a38bf7f6d648
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-17 20:19:08 -0300

    Redirect to login after register

--- containers/RegisterContainer/RegisterContainer.tsx
+++ containers/RegisterContainer/RegisterContainer.tsx
@@ -1,6 +1,7 @@
 import { useState, useCallback, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useContextSelector } from 'use-context-selector';
+import { useRouter } from 'next/router';
 import { Tabs } from '@cig-platform/ui';
 
 import RegisterUserForm from '@Components/Register/RegisterUserForm/RegisterUserForm';
@@ -9,9 +10,12 @@ import useSubmitRegister from '@Hooks/useSubmitRegister';
 import { error as showError, success } from '@Utils/alert';
 import RegisterContext from '@Contexts/RegisterContext/RegisterContext';
 import { selectError } from '@Contexts/RegisterContext/registerSelectors';
+import { Routes } from '@Constants/routes';
 
 export default function RegisterContainer() {
   const [tab, setTab] = useState(0);
+
+  const router = useRouter();
 
   const error = useContextSelector(RegisterContext, selectError);
 
@@ -21,7 +25,12 @@ export default function RegisterContainer() {
     setTab(1);
   }, []);
 
-  const handleSuccessForm = useCallback(() => success(t('common.success-registered'), t), [t]);
+  const handleSuccessForm = useCallback(() => 
+    success(
+      t('common.success-registered'),
+      t,
+      () => router.push(Routes.Login)
+    ), [t]);
 
   const handleSubmitRegister = useSubmitRegister({ onSuccess: handleSuccessForm });
 
