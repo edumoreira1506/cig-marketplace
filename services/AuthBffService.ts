b1300d0108f86a72f71274a97874554302a1a47e
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-16 22:21:04 -0300

    Add removeNullProperties

--- services/AuthBffService.ts
+++ services/AuthBffService.ts
@@ -2,11 +2,12 @@ import AuthBffClient from '@cig-platform/auth-bff-client';
 
 import { RegisterState } from '@Contexts/RegisterContext/registerReducer';
 import { AUTH_BFF_URL } from '@Constants/urls';
+import { removeNullProperties } from '@Utils/object';
 
 const authBffClient = new AuthBffClient(AUTH_BFF_URL);
 
 export default class AuthBffService {
-  static registerUser({ user, poultry }: { user: RegisterState['user']; poultry: RegisterState['poultry'] }) {
-    return authBffClient.registerUser(user as any, poultry as any);
+  static registerUser({ user, poultry }: { user: Partial<RegisterState['user']>; poultry: Partial<RegisterState['poultry']> }) {
+    return authBffClient.registerUser(removeNullProperties(user) as any, removeNullProperties(poultry) as any);
   }
 }
