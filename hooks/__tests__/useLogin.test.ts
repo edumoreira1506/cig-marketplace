4524b5b512f5e2dbc3e58a5a80853e4600af41e1
Eduardo Moreira <contato@eduardoem.com.br>
2021-09-18 10:10:30 -0300

    Add tests to hooks

---
+++ hooks/__tests__/useLogin.test.ts
@@ -0,0 +1,61 @@
+import * as actions from '../../contexts/LoginContext/loginActions';
+import { INITIAL_STATE } from '../../contexts/LoginContext/loginReducer';
+import { createLoginContextHookRenderer } from '../../utils/tests/loginContextRenderer';
+import AuthBffService from '../../services/AuthBffService';
+
+import useLogin from '../useLogin';
+
+describe('useLogin', () => {
+  it('has the correct behavior when gets a valid response', async () => {
+    const mockStore = {
+      ...INITIAL_STATE,
+      email: 'email@exemplo.com',
+      password: 'password'
+    };
+    const mockSetIsLoading = jest.fn();
+    const mockSetError = jest.fn();
+    const mockLogin = jest.fn().mockResolvedValue({ ok: true });
+    const onSuccess = jest.fn();
+    const renderHook = createLoginContextHookRenderer(mockStore);
+
+    jest.spyOn(AuthBffService, 'login').mockImplementation(mockLogin);
+    jest.spyOn(actions, 'setIsLoading').mockImplementation(mockSetIsLoading);
+    jest.spyOn(actions, 'setError').mockImplementation(mockSetError);
+
+    const { result } = renderHook(() => useLogin({ onSuccess }));
+
+    await (result.current as any)?.(mockStore.email, mockStore.password);
+
+    expect(mockSetIsLoading).toHaveBeenCalledTimes(2);
+    expect(mockSetError).not.toHaveBeenCalled();
+    expect(mockLogin).toHaveBeenCalledWith({ email: mockStore.email, password: mockStore.password });
+    expect(onSuccess).toHaveBeenCalled();
+  });
+
+  it('has the correct behavior when gets a invalid response', async () => {
+    const mockStore = {
+      ...INITIAL_STATE,
+      email: 'email@exemplo.com',
+      password: 'password'
+    };
+    const error = {};
+    const mockSetIsLoading = jest.fn();
+    const mockSetError = jest.fn();
+    const mockLogin = jest.fn().mockResolvedValue({ ok: false, error });
+    const onSuccess = jest.fn();
+    const renderHook = createLoginContextHookRenderer(mockStore);
+
+    jest.spyOn(AuthBffService, 'login').mockImplementation(mockLogin);
+    jest.spyOn(actions, 'setIsLoading').mockImplementation(mockSetIsLoading);
+    jest.spyOn(actions, 'setError').mockImplementation(mockSetError);
+
+    const { result } = renderHook(() => useLogin({ onSuccess }));
+
+    await (result.current as any)?.(mockStore.email, mockStore.password);
+
+    expect(mockSetIsLoading).toHaveBeenCalledTimes(2);
+    expect(mockSetError).toHaveBeenCalledWith(error);
+    expect(mockLogin).toHaveBeenCalledWith({ email: mockStore.email, password: mockStore.password });
+    expect(onSuccess).not.toHaveBeenCalled();
+  });
+});
