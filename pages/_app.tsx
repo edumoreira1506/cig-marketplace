2a981cca270facad913f790cb99e479ba3e7df6b
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-05 22:00:55 -0300

    Add AppContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
 import { ReactElement } from 'react';
 
+import { AppProvider } from '@Contexts/AppContext/AppContext';
+
 import '@Styles/globals.css';
 import '@Configs/i18n';
 
 export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider>
+      <Component {...pageProps} />;
+    </AppProvider>
+  );
 }
