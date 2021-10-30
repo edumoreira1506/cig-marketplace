8aa501819791843cc9476de547d9becc02741d85
Eduardo Moreira <contato@eduardoem.com.br>
2021-10-30 10:57:21 -0300

    Add length code (#17)
    * Bump @cig-platform/ui to 0.1.56
    
    * Add .eslintignore
    
    * Remove default structure from next js
    
    * Update code field
    
    * Add matchMedia

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,9 @@
 import '@testing-library/jest-dom/extend-expect';
 import '@Configs/i18n';
+
+global.matchMedia = global.matchMedia || function () {
+  return {
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+};
