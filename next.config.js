981adf315ac74792ee9712a76f1d2cf6680d4221
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-20 20:30:06 -0300

    Fix build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const { withGlobalCss } = require('next-global-css')
+
+const withConfig = withGlobalCss()
+
+module.exports = withConfig({
   reactStrictMode: true,
   pageExtensions: ['tsx'],
-};
+});
