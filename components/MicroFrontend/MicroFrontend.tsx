0b80349ed536b66406e9d2d44530ce7a8dbbcb5e
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-20 19:02:01 -0300

    Add poultry page

---
+++ components/MicroFrontend/MicroFrontend.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+
+export interface MicroFrontendProps {
+  name: string;
+  host: string;
+  containerId: string;
+  breederId?: string;
+  poultryId?: string;
+}
+
+export default function MicroFrontend({
+  name,
+  host,
+  containerId,
+  breederId,
+  poultryId
+}: MicroFrontendProps) {
+  useEffect(() => {
+    const renderMicroFrontend = () => {
+      const windowRender = (window as any)?.[`render${name}`];
+
+      if (windowRender) {
+        if (breederId && !poultryId) {
+          windowRender(containerId, breederId);
+        }
+        
+        if (breederId && poultryId) {
+          windowRender(containerId, breederId, poultryId);
+        }
+      }
+    };
+
+    fetch(`${host}/asset-manifest.json`)
+      .then((res) => res.json())
+      .then((manifest) => {
+        const script = document.createElement('script');
+
+        script.id = `script-${containerId}`;
+        script.crossOrigin = '';
+        script.src = `${host}${manifest.files['main.js']}`;
+        script.onload = () => {
+          renderMicroFrontend();
+        };
+        document.head.appendChild(script);
+      });
+
+    return () => {
+      (window as any)[`unmount${name}`] && (window as any)[`unmount${name}`](`${name}-container`);
+    };
+  }, [name, host, containerId, poultryId]);
+
+  return <main id={`${name}-container`} />;
+}
--- constants/urls.ts
+++ constants/urls.ts
@@ -1,2 +1,4 @@
 export const AUTH_BFF_URL = String(process.env.NEXT_PUBLIC_AUTH_BFF_URL);
 export const BACKOFFICE_URL = String(process.env.NEXT_PUBLIC_BACKOFFICE_URL);
+
+export const BREEDER_PAGE_URL = String(process.env.NEXT_PUBLIC_BREEDER_PAGE_URL);
