3048f417304fca1d33afd5e0feff17442344335d
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-20 21:27:36 -0300

    Add unauthorized items

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,39 @@ import type { AppProps } from 'next/app';
 import { ReactElement, useCallback, useMemo } from 'react';
 import { Container as UiContainer } from '@cig-platform/ui';
 import { BiLogIn } from 'react-icons/bi';
+import { GiChicken } from 'react-icons/gi';
 
 import { AppProvider } from '@Contexts/AppContext/AppContext';
 import Container from '@Components/Shared/Container/Container';
 import '@Configs/i18n';
 import useUser from '@Hooks/useUser';
 import { useRouter } from 'next/router';
+import { AiFillHome } from 'react-icons/ai';
+import { BACKOFFICE_URL } from '@Constants/urls';
 
 const unauthorizedItems = [
   {
     title: 'Login',
     icon: <BiLogIn />,
     route: '/login'
+  },
+];
+
+export const authorizedItems = [
+  {
+    title: 'Home',
+    icon: <AiFillHome />,
+    route: `${BACKOFFICE_URL}`
+  },
+  {
+    title: 'Meu criatório',
+    icon: <AiFillHome />,
+    route: `${BACKOFFICE_URL}criatorio`
+  },
+  {
+    title: 'Minhas aves',
+    icon: <GiChicken />,
+    route:  `${BACKOFFICE_URL}meu-plantel`
   },
 ];
 
@@ -24,10 +45,10 @@ export default function MyApp({ Component, pageProps }: AppProps): ReactElement 
 
   const isAuthenticated = Boolean(user.name);
 
-  const items = useMemo(() => isAuthenticated ? [] : unauthorizedItems, [isAuthenticated]);
+  const items = useMemo(() => isAuthenticated ? authorizedItems : unauthorizedItems, [isAuthenticated]);
 
   const handleMenuItemClick = useCallback((menuItemTitle: string) => {
-    const itemRoute = unauthorizedItems.find(i => i.title === menuItemTitle)?.route;
+    const itemRoute = [...unauthorizedItems, ...authorizedItems].find(i => i.title === menuItemTitle)?.route;
 
     if (itemRoute) {
       router.push(itemRoute);
