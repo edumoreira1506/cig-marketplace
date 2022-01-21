7a5978c1819230376da2b755f0c7af4d830f5440
Eduardo Moreira <contato@eduardoem.com.br>
2022-01-20 21:18:52 -0300

    Add menu

---
+++ hooks/useUser.ts
@@ -0,0 +1,33 @@
+import { useLocalStorage } from '@cig-platform/hooks';
+import { IBreeder, IUser } from '@cig-platform/types';
+import { useMemo } from 'react';
+import jwt from 'jsonwebtoken';
+
+interface IDecodedToken {
+  breeders: IBreeder[];
+  email: IUser['email'];
+  id: IUser['id'];
+  name: IUser['name'];
+}
+
+export default function useUser() {
+  const { get } = useLocalStorage('token');
+
+  return useMemo(() => {
+    try {
+      const localStorageToken = get();
+
+      const decodedToken = jwt.decode(localStorageToken) as IDecodedToken;
+  
+      return {
+        ...decodedToken,
+        image: 'https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png',
+      };
+    } catch {
+      return { 
+        image: 'https://www.nicepng.com/png/detail/128-1280406_view-user-icon-png-user-circle-icon-png.png',
+        name: ''
+      };
+    }
+  }, [get]);
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,53 @@
 import type { AppProps } from 'next/app';
-import { ReactElement } from 'react';
+import { ReactElement, useCallback, useMemo } from 'react';
+import { Container as UiContainer } from '@cig-platform/ui';
+import { BiLogIn } from 'react-icons/bi';
 
 import { AppProvider } from '@Contexts/AppContext/AppContext';
 import Container from '@Components/Shared/Container/Container';
-
 import '@Configs/i18n';
+import useUser from '@Hooks/useUser';
+import { useRouter } from 'next/router';
+
+const unauthorizedItems = [
+  {
+    title: 'Login',
+    icon: <BiLogIn />,
+    route: '/login'
+  },
+];
 
 export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
+  const user = useUser();
+
+  const router = useRouter();
+
+  const isAuthenticated = Boolean(user.name);
+
+  const items = useMemo(() => isAuthenticated ? [] : unauthorizedItems, [isAuthenticated]);
+
+  const handleMenuItemClick = useCallback((menuItemTitle: string) => {
+    const itemRoute = unauthorizedItems.find(i => i.title === menuItemTitle)?.route;
+
+    if (itemRoute) {
+      router.push(itemRoute);
+    }
+  }, [router]);
+
   return (
     <AppProvider>
-      <Container>
-        <Component {...pageProps} />
-      </Container>
+      <UiContainer
+        items={items}
+        onMenuClick={handleMenuItemClick}
+        onShortcutClick={() => null}
+        shortcuts={[]}
+        title='CIG Marketplace'
+        user={user}
+      >
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </UiContainer>
     </AppProvider>
   );
 }
