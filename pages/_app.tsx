a31d7baa1e34c7c63e32b2fddffcef86a32cb7fe
Eduardo Moreira <contato@eduardoem.com.br>
2022-02-07 21:51:52 -0300

    Chicken 387 (#30)
    * Bump @cig-platform/ui to 0.1.106
    
    * Move logic to Container
    
    * Add isLoading as prop of Container
    
    * Fix import sort

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,103 +1,16 @@
 import type { AppProps } from 'next/app';
-import { ReactElement, useCallback, useMemo } from 'react';
-import { Container as UiContainer } from '@cig-platform/ui';
-import { BiLogIn } from 'react-icons/bi';
-import { GiChicken, GiReceiveMoney } from 'react-icons/gi';
+import { ReactElement } from 'react';
 
 import { AppProvider } from '@Contexts/AppContext/AppContext';
 import Container from '@Components/Shared/Container/Container';
 import '@Configs/i18n';
-import useUser from '@Hooks/useUser';
-import { useRouter } from 'next/router';
-import { AiFillHome, AiOutlinePoweroff } from 'react-icons/ai';
-import { BACKOFFICE_URL, LOGO_URL } from '@Constants/urls';
-
-const unauthorizedItems = [
-  {
-    title: 'Login',
-    icon: <BiLogIn />,
-    route: '/login'
-  },
-];
-
-export const authorizedItems = [
-  {
-    title: 'Home',
-    icon: <AiFillHome />,
-    route: `${BACKOFFICE_URL}`
-  },
-  {
-    title: 'Meu criatório',
-    icon: <AiFillHome />,
-    route: `${BACKOFFICE_URL}criatorio`
-  },
-  {
-    title: 'Minhas aves',
-    icon: <GiChicken />,
-    route:  `${BACKOFFICE_URL}meu-plantel`
-  },
-  {
-    title: 'Vendas',
-    icon: <GiReceiveMoney />,
-    route: `${BACKOFFICE_URL}vendas`
-  },
-  {
-    title: 'Compras',
-    icon: <GiReceiveMoney />,
-    route: `${BACKOFFICE_URL}compras`
-  },
-  {
-    title: 'Sair',
-    icon: <AiOutlinePoweroff />,
-    route: `${BACKOFFICE_URL}logout`
-  }
-];
-
-const shortcuts = ['Sair', 'Editar senha'];
-
-const shortcutLinks = {
-  [shortcuts[0]]: `${BACKOFFICE_URL}logout`,
-  [shortcuts[1]]: `${BACKOFFICE_URL}editar-senha`,
-};
 
 export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  const user = useUser();
-
-  const router = useRouter();
-
-  const isAuthenticated = Boolean(user.name);
-
-  const items = useMemo(() => isAuthenticated ? authorizedItems : unauthorizedItems, [isAuthenticated]);
-
-  const shurtcutItems = useMemo(() => isAuthenticated ? shortcuts : [], [isAuthenticated]);
-
-  const handleMenuItemClick = useCallback((menuItemTitle: string) => {
-    const itemRoute = [...unauthorizedItems, ...authorizedItems].find(i => i.title === menuItemTitle)?.route;
-
-    if (itemRoute) {
-      router.push(itemRoute);
-    }
-  }, [router]);
-
-  const handleShortcutClick = useCallback((shortcut: string) => {
-    router.push(shortcutLinks[shortcut]);
-  }, [router]);
-
   return (
     <AppProvider>
-      <UiContainer
-        items={items}
-        onMenuClick={handleMenuItemClick}
-        onShortcutClick={handleShortcutClick}
-        shortcuts={shurtcutItems}
-        title='CIG Marketplace'
-        user={user}
-        logoUrl={LOGO_URL}
-      >
-        <Container>
-          <Component {...pageProps} />
-        </Container>
-      </UiContainer>
+      <Container>
+        <Component {...pageProps} />
+      </Container>
     </AppProvider>
   );
 }
