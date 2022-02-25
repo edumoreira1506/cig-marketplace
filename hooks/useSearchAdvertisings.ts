7c3846f313d977bd2744420c64f7391ed7dbd0a9
Eduardo Moreira <contato@eduardoem.com.br>
2022-02-24 21:18:34 -0300

    Fix build

--- hooks/useSearchAdvertisings.ts
+++ hooks/useSearchAdvertisings.ts
@@ -46,6 +46,7 @@ export default function useSearchAdvertisngs() {
           sort,
           tail,
           type,
+          favorites: []
         });
 
         setAdvertisingsData(advertisings);
