93aefe0d4ebc767383d57c8fd966a7a3f68f37c8
Eduardo Moreira <contato@eduardoem.com.br>
2022-03-09 16:27:01 -0300

    Remove unnused import

--- components/Shared/Container/Container.tsx
+++ components/Shared/Container/Container.tsx
@@ -4,7 +4,7 @@ import { Container as UiContainer } from '@cig-platform/ui';
 import { useRouter } from 'next/router';
 import { BiLogIn } from 'react-icons/bi';
 import { AiFillHome, AiOutlinePoweroff } from 'react-icons/ai';
-import { GiChicken, GiReceiveMoney } from 'react-icons/gi';
+import { GiReceiveMoney } from 'react-icons/gi';
 
 import { useAppSelector } from '@Contexts/AppContext/AppContext';
 import { selectError, selectIsLoading } from '@Contexts/AppContext/appSelectors';
