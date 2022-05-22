7155d9a0b1183bdc1a4c684e93db814f1d65e4fc
Eduardo Moreira <contato@eduardoem.com.br>
2022-05-22 12:49:37 -0300

    Add date picker css import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import '@Configs/i18n';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
