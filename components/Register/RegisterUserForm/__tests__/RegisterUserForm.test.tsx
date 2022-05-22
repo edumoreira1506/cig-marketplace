83c685bb3b63f2e05a650eea75bc5da4d7bd5633
Eduardo Moreira <contato@eduardoem.com.br>
2022-05-22 13:03:16 -0300

    Change dynamic value for a hardcoded

--- components/Register/RegisterUserForm/__tests__/RegisterUserForm.test.tsx
+++ components/Register/RegisterUserForm/__tests__/RegisterUserForm.test.tsx
@@ -54,7 +54,7 @@ describe('RegisterUserForm', () => {
     expect(screen.getByDisplayValue(mockStore.user.email)).toBeInTheDocument();
     expect(screen.getByDisplayValue(mockStore.user.name)).toBeInTheDocument();
     expect(screen.getByDisplayValue(mockStore.user.register)).toBeInTheDocument();
-    expect(screen.getByDisplayValue(new Date(mockStore.user.birthDate).toLocaleDateString())).toBeInTheDocument();
+    expect(screen.getByDisplayValue('01/01/2001')).toBeInTheDocument();
   });
 
   it('calls onSubmit', () => {
