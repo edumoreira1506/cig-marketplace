a9fc3ab6107e688919b3edd56818118ff9ec0154
edumoreira1506 <contato@eduardoem.com.br>
2022-05-31 17:44:20 -0300

    Fix HomeContainer test

--- containers/HomeContainer/__tests__/HomeContainer.spec.tsx
+++ containers/HomeContainer/__tests__/HomeContainer.spec.tsx
@@ -1,6 +1,7 @@
 import { PoultryGenderCategoryEnum, PoultryGenderEnum, RegisterTypeEnum } from '@cig-platform/enums';
 import { advertisingFactory, breederFactory, poultryFactory } from '@cig-platform/factories';
 import { render, screen, waitFor } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import HomeContainer from '../HomeContainer';
 
@@ -9,21 +10,6 @@ const advertising = advertisingFactory();
 const femalePoultry = poultryFactory({
   gender: PoultryGenderEnum.Female,
   genderCategory: PoultryGenderCategoryEnum.FemaleChicken,
-});
-
-const matrixPoultry = poultryFactory({
-  gender: PoultryGenderEnum.Female,
-  genderCategory: PoultryGenderCategoryEnum.Matrix
-});
-
-const reproductivePoultry = poultryFactory({
-  gender: PoultryGenderEnum.Male,
-  genderCategory: PoultryGenderCategoryEnum.Reproductive
-});
-
-const malePoultry = poultryFactory({
-  gender: PoultryGenderEnum.Male,
-  genderCategory: PoultryGenderCategoryEnum.MaleChicken
 });
 
 const defaultRegister = {
@@ -40,64 +26,40 @@ const defaultRegister = {
 };
 
 const DEFAULT_PROPS = {
-  advertisings: {
-    femaleChickens: [{
-      poultry: femalePoultry as any,
-      advertising,
-      breeder,
-      measurementAndWeight: defaultRegister
-    }],
-    maleChickens: [{
-      poultry: malePoultry  as any,
-      advertising,
-      breeder,
-      measurementAndWeight: {
-        ...defaultRegister,
-        metadata: {
-          measurement: '160',
-          weight: '160'
+  carousels: [
+    {
+      title: 'Título teste',
+      identifier: 'something',
+      advertisings: [
+        {
+          poultry: femalePoultry as any,
+          advertising,
+          breeder,
+          measurementAndWeight: {
+            ...defaultRegister,
+            metadata: {
+              measurement: '160',
+              weight: '160'
+            }
+          }
         }
-      }
-    }],
-    matrixes: [{
-      poultry: matrixPoultry  as any,
-      advertising,
-      breeder,
-      measurementAndWeight: {
-        ...defaultRegister,
-        metadata: {
-          measurement: '170',
-          weight: '170'
-        }
-      }
-    }],
-    reproductives: [{
-      poultry: reproductivePoultry  as any,
-      advertising,
-      breeder,
-      measurementAndWeight: {
-        ...defaultRegister,
-        metadata: {
-          measurement: '180',
-          weight: '180'
-        }
-      }
-    }]
-  }
+      ]
+    }
+  ]
 };
+
+const queryClient = new QueryClient();
 
 describe('<HomeContainer />', () => {
   it('renders correctly', async () => {
-    render(<HomeContainer {...DEFAULT_PROPS} />);
+    render(
+      <QueryClientProvider client={queryClient}>
+        <HomeContainer {...DEFAULT_PROPS} />
+      </QueryClientProvider>
+    );
 
-    await waitFor(() => expect(screen.getByText('Matrizes')).toBeInTheDocument());
+    await waitFor(() => expect(screen.getByText(DEFAULT_PROPS.carousels[0].title)).toBeInTheDocument());
 
-    expect(screen.getByText('Reprodutores')).toBeInTheDocument();
-    expect(screen.getByText('Frangos')).toBeInTheDocument();
-    expect(screen.getByText('Frangas')).toBeInTheDocument();
-    expect(screen.getByText(`${new Intl.DateTimeFormat('pt-BR').format(new Date(femalePoultry.birthDate))} - ${DEFAULT_PROPS.advertisings.femaleChickens[0].measurementAndWeight.metadata.measurement} CM`)).toBeInTheDocument();
-    expect(screen.getByText(`${new Intl.DateTimeFormat('pt-BR').format(new Date(malePoultry.birthDate))} - ${DEFAULT_PROPS.advertisings.maleChickens[0].measurementAndWeight.metadata.measurement} CM`)).toBeInTheDocument();
-    expect(screen.getByText(`${new Intl.DateTimeFormat('pt-BR').format(new Date(matrixPoultry.birthDate))} - ${DEFAULT_PROPS.advertisings.matrixes[0].measurementAndWeight.metadata.measurement} CM`)).toBeInTheDocument();
-    expect(screen.getByText(`${new Intl.DateTimeFormat('pt-BR').format(new Date(reproductivePoultry.birthDate))} - ${DEFAULT_PROPS.advertisings.reproductives[0].measurementAndWeight.metadata.measurement} CM`)).toBeInTheDocument();
+    expect(screen.getByText(`${new Intl.DateTimeFormat('pt-BR').format(new Date(femalePoultry.birthDate))} - ${DEFAULT_PROPS.carousels[0].advertisings[0].measurementAndWeight.metadata.measurement} CM`)).toBeInTheDocument();
   });
 });
