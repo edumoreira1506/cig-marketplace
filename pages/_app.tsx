db304e457afac73dec793e720123387cbae8b3f2
edumoreira1506 <contato@eduardoem.com.br>
2022-09-16 17:15:46 -0300

    Fix icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,20 +29,21 @@ export default function MyApp({ Component, pageProps }: AppProps): ReactElement 
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#2a3c5a" />
 
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-        <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/logo.png" />
 
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo.png" />
+
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#2a3c5a" />
+
+        <link rel="mask-icon" href="/logo.svg" color="#2a3c5a" />
+
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
         <meta name="twitter:title" content="CIG Marketplace" />
         <meta name="twitter:description" content="" />
 
