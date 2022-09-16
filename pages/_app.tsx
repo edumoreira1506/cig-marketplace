74df14e608aa04b842d21eaf3fbd0ff3a2888505
Eduardo Moreira <contato@eduardoem.com.br>
2022-09-16 17:25:50 -0300

    Chicken 995 (#92)
    * Add next-pwa
    
    * Setup next-pwa
    
    * Add manifest.json
    
    * Add metatags
    
    * Fix icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,15 +11,54 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import 'react-datepicker/dist/react-datepicker.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
-    <QueryClientProvider client={queryClient}>
-      <AppProvider>
-        <Container>
-          <Component {...pageProps} />
-        </Container>
-      </AppProvider>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <meta name="application-name" content="CIG Marketplace" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="CIG Marketplace" />
+        <meta name="description" content="" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#2a3c5a" />
+
+        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/logo.png" />
+
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo.png" />
+
+        <link rel="manifest" href="/manifest.json" />
+
+        <link rel="mask-icon" href="/logo.svg" color="#2a3c5a" />
+
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+
+        <meta name="twitter:title" content="CIG Marketplace" />
+        <meta name="twitter:description" content="" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="CIG Marketplace" />
+        <meta property="og:site_name" content="CIG Marketplace" />
+      </Head>
+
+      <QueryClientProvider client={queryClient}>
+        <AppProvider>
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+        </AppProvider>
+      </QueryClientProvider>
+    </>
   );
 }
